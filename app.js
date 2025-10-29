2c35ad4176c6e40d808c20aa0c139a56506196da
trevorwinser <64448847+trevorwinser@users.noreply.github.com>
2025-10-29 12:35:45 -0700

    scrapped generic page | made fun landing page

--- app.js
+++
@@ -1,9 +0,0 @@
-function consolidateNav() {
-  var x = document.getElementById("topnav-links");
-    if (x.className === "topnav-links") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav-links";
-    }
-  console.log("BLAH")
-}
